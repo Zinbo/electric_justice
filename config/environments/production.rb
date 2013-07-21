44b33841cf65066c3745e15e58ceac29470587e1
Zinbo <superzinbo@gmail.com>
2013-07-21 16:45:35 +0100

    Add smtp development settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,14 @@ ElectricJustice::Application.configure do
   
   config.action_mailer.default_url_options = { :host => 'electric-justice.herokuapp.com' }
   
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => ENV['EJ_EMAIL_USERNAME'],
+    :password => ENV['EJ_EMAIL_PASSWORD']
+  }
+  
 end
